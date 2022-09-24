dc365a9ab71037182fd48963e8467d8875f7e51a
Ata Can Çalışkan <caliskanataa@gmail.com>
2022-09-24 13:54:52 +0300

    Next-Learning

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+            </Head>
+            <div>
+                <h1>
+                    About Page
+                </h1>
+                <p>Cillum pariatur qui laborum aute qui eu aute ipsum velit laborum sit veniam. Deserunt id labore nisi sint mollit adipisicing sit nulla culpa qui in cupidatat. Ad velit ea irure dolore Lorem qui voluptate. Non laboris enim eiusmod amet deserunt et aliqua in ullamco. Quis culpa aute dolore occaecat duis dolor veniam dolore. Consectetur ea duis Lorem aute.</p>
+                <p>Cillum pariatur qui laborum aute qui eu aute ipsum velit laborum sit veniam. Deserunt id labore nisi sint mollit adipisicing sit nulla culpa qui in cupidatat. Ad velit ea irure dolore Lorem qui voluptate. Non laboris enim eiusmod amet deserunt et aliqua in ullamco. Quis culpa aute dolore occaecat duis dolor veniam dolore. Consectetur ea duis Lorem aute.</p>
+            </div>
+        </>
+    );
+}
+ 
+export default About;
